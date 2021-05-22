ea3d5207e4dc72061c6931a8ae1b988598a6edb9
tetsuji1000 <kontetsu0206@gmail.com>
2021-05-22 12:09:11 +0900

    front/header,footer create

---
+++ front/components/layouts/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer color="cyan darken-4" dark app>
+    <v-col class="text-center">
+    2021 Crearn City
+    </v-col>
+  </v-footer> 
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
