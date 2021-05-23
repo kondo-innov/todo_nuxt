c9c3222ffe7896f6f4041a34ad866596edff4ab0
tetsuji1000 <kontetsu0206@gmail.com>
2021-05-23 09:58:00 +0900

    devise_token_auth setting

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
-end
+        skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
+      end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
