2d62bbe733e729614d25defd6363942f53e48cb4
tetsuji1000 <kontetsu0206@gmail.com>
2021-06-17 23:41:09 +0900

    front/login 不具合修正

--- front/pages/users/signup.vue
+++ front/pages/users/signup.vue
@@ -143,7 +143,6 @@ export default {
                   return error
                 }
               )
-              window.location.href = '/'
             })
           .catch((error) => {
             debugger;
