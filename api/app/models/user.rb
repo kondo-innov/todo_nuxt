429cae53de7821a37cb30ebdb16af35fdb58e93d
tetsuji1000 <kontetsu0206@gmail.com>
2021-06-22 12:42:06 +0900

    api/gem 'carrierwave' delete

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -7,5 +7,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  mount_uploader :image, ImageUploader
 end
