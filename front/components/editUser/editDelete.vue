6fa6d9b76b06bf340e9d7b265e546c9a70db0a69
tetsuji1000 <your.email@example.com>
2021-07-03 13:22:48 +0900

    front/delete.vue

---
+++ front/components/editUser/editDelete.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-3">
+    <v-form ref="form">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" md="10" sm="10">
+            <v-btn
+              block
+              class="mr-4 blue white--text"
+              @click="deleteUser"
+            >
+            削除
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({}),
+  methods: {
+    deleteUser() {
+      this.$axios
+        .delete('api/v1/auth', {
+          headers: {
+            'access-token': localStorage.getItem('access-token'),
+            uid: localStorage.getItem('uid'),
+            client: localStorage.getItem('client'),
+          },
+        })
+        .then((response) => {
+          this.$auth.logout()
+          window.location.href = '/'
+        })
+    },
+  },
+}
+</script>
