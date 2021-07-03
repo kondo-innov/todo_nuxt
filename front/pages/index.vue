cd96575d4339658f683693096dc29a4c2d43423e
tetsuji1000 <kontetsu0206@gmail.com>
2021-07-03 16:26:01 +0900

    front/edit email

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,39 +1,9 @@
 <template>
-  <div class="mt-3">
-    <v-card class="mt-5 mx-auto" max-width="600">
-      <v-form ref="form">
-        <v-container>
-          <v-row justify="center">
-            <p cols="12" class="mt-3 display-1 grey--text">
-              login
-            </p>
-          </v-row>
-          <v-row justify="center">
-            <v-col cols="12" md="10" sm="10">
-              <v-btn
-                block
-                class="mr-4 blue white--text"
-                @click="logout()"
-              >
-                logout
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </v-card>
-  </div>
+  
 </template>
 
 <script>
 export default {
-  methods: {
-    logout() {
-      this.$auth.logout();
-    },
-  }
+
 }
 </script>
-
-<style lang="scss">
-</style>
