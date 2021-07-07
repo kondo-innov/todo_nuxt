95894db45fa7e0c1a5779d2073dcacb5db004ed8
tetsuji1000 <your.email@example.com>
2021-07-07 20:15:27 +0900

    directuploader delete create new

--- front/components/editUser/editAvatar.vue
+++ front/components/editUser/editAvatar.vue
@@ -23,7 +23,7 @@
           id="image-files"
           ref="image_files"
           type="file"
-          @change="onChangeFiles"
+          @change="setImage"
         />
       </v-col>
     </v-row>
@@ -31,71 +31,32 @@
 </template>
 
 <script>
-  import DirectUploader from '@/plugins/DirectUploader'
   export default {
     data(){
       return {
-        files: [],
+        imageFile: [],
         input_image: null,
         defaultImg: require("@/assets/images/default_user_icon.jpeg")
       }
     },
     methods: {
-      onChangeFiles (_event) {
-        const input = this.$refs.image_files
-        this.uploadFiles(input.files)
-        // 選択されたファイルを入力からクリアしておく
-        input.value = null
+      setImage(e) {
+        e.preventDefault();
+        this.imageFile = e.target.files[0];
+        this.upload()
       },
-      uploadFiles (files) {
-        this.files = []
-        Array.from(files).forEach(file => this.uploadFile(file))
-      },
-      uploadFile (file) {
-        // アップロードを実行
-        const filename = file.name
-        const tmpUploadFile = { name: filename, fileSize: file.size, progress: 0, isUploading: true }
-        this.files.push(tmpUploadFile)
-        const url = 'http://localhost:3000/rails/active_storage/direct_uploads'
-        const directUploader = new DirectUploader(file, url, this._onUploadProgress.bind(this))
-        directUploader.upload(this._onUploadError.bind(this), this._onUploadSuccess.bind(this))
-      },
-      _onUploadError (error, directUploader) {
-        // アップロード失敗時の処理
-        const params = { isUploading: false, message: error }
-        const filename = directUploader.file.name
-        this._updateFiles(filename, params)
-      },
-      async _onUploadSuccess (blob, directUploader) {
-        // アップロード成功時の処理
-        const filename = directUploader.file.name
-        const params = { isUploading: false, message: 'アップロード完了' }
-        const postParams = { filename: blob.filename, image: blob.signed_id }
-        const url = 'http://localhost:3000/api/v1/avatars'
-        try {
-          await this.$axios.$post(url, postParams)
-          this._updateFiles(filename, params)
-        } catch (error) {
-          const params = { isUploading: false, message: error }
-          this._updateFiles(filename, params)
+      async upload() {
+        let formData = new FormData();
+        if (this.imageFile !== null) {
+          formData.append("image", this.imageFile);
         }
+        this.createPost(formData);
       },
-      _onUploadProgress (event, directUploader) {
-        // アップロード中の処理
-        const progress = this._calcProgress(event)
-        const params = { progress }
-        const filename = directUploader.file.name
-        this._updateFiles(filename, params)
+      async createPost(post) {
+        const url = 'http://localhost:3000/api/v1/avatars'
+        const response = await this.$axios.post(url, post)
+        console.log(response)
       },
-      _updateFiles (filename, params) {
-        this.files = this.files.map((file) => {
-          if (file.name !== filename) { return file }
-          return Object.assign(file, params)
-        })
-      },
-      _calcProgress (event) {
-        return (event.loaded / event.total) * 100
-      }
     }
   }
 </script>
