2345a37dc7cf51aa4832e34e29a7934b90ae2b4e
tetsuji1000 <your.email@example.com>
2021-07-16 13:01:51 +0900

    front/eventpost add eventname add

---
+++ front/components/menu/post/eventpostingPage/eventName.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-text-field
+      v-model="eventname"
+      :rules="nameRules"
+      :counter="10"
+      label="イベント名"
+      prepend-icon="mdi-account"
+      required
+    ></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      eventname:       '',
+      nameRules: [
+        v => !!v || 'イベント名を記入してください',
+        v => v.length <= 10 || '10文字以内で入力してください',
+      ],
+    }
+  },
+}
+</script>
