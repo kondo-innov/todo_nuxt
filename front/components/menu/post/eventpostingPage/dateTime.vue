6af92fb1a095415386a8db081a5e541c2e66f6fb
tetsuji1000 <your.email@example.com>
2021-07-16 20:35:21 +0900

    front/dateTime add

---
+++ front/components/menu/post/eventpostingPage/dateTime.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div>
+      <VueCtkDateTimePicker v-model="timeValue"></VueCtkDateTimePicker>    </div>
+  </div>
+</template>
+ 
+<script> 
+export default {
+  data() {
+    return {
+      timeValue: "2021-01-01 00:00"
+    }
+  }
+}
+</script>
--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -32,7 +32,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/axios.js', ssr: false }
+    { src: '~/plugins/axios.js', ssr: false, 
+      src: '@/plugins/plugin',
+      mode: 'client'
+    }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
