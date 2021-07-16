e384b14b7bc3a7ce16db2f93d0f8edf10ee1cc00
tetsuji1000 <kontetsu0206@gmail.com>
2021-07-16 13:01:51 +0900

    front/eventpost add eventname add

---
+++ front/components/menu/post/eventpostingPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="8"
+        >
+        <eventName />
+        </v-col>
+        <v-col>
+
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+import eventName from "~/components/menu/post/eventpostingPage/eventName.vue"
+
+export default {
+  components: {
+    eventName,
+  },
+  data () {
+    return {
+      valid:           false,
+    }
+  },
+}
+</script>
