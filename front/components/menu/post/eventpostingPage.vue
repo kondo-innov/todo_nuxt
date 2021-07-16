7688aa17fe6c1dd03c68ebeac8bf23b8d12bbb28
tetsuji1000 <kontetsu0206@gmail.com>
2021-07-16 20:35:21 +0900

    front/dateTime add

--- front/components/menu/post/eventpostingPage.vue
+++ front/components/menu/post/eventpostingPage.vue
@@ -1,26 +1,33 @@
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="8"
-        >
-        <eventName />
-        </v-col>
-        <v-col>
-
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+  <v-app>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="8"
+          >
+          <eventName />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            cols="8"
+          >
+          <dateTime />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-app>
 </template>
 
 <script>
 import eventName from "~/components/menu/post/eventpostingPage/eventName.vue"
-
+import dateTime  from "~/components/menu/post/eventpostingPage/dateTime.vue"
 export default {
   components: {
     eventName,
+    dateTime,
   },
   data () {
     return {
