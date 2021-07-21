8f653cfdb3487aa463de1b32063552ecd0256616
tetsuji1000 <kontetsu0206@gmail.com>
2021-07-21 14:13:43 +0900

    front/avatar if change

--- api/app/controllers/api/v1/current_users_controller.rb
+++ api/app/controllers/api/v1/current_users_controller.rb
@@ -2,7 +2,11 @@ module Api
   module V1
     class CurrentUsersController < ApplicationController
       def show
-        render json: { url: current_user.image.service_url, messages: '成功' }, stauts: :ok
+        if current_user.image.attached?
+          render json: { url: current_user.image.service_url, messages: '成功' }, stauts: :ok
+        else 
+          render json: {messages: '失敗' }, stauts: :ok 
+        end
       end 
     end
   end
