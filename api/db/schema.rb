ac63bc8500dc13a7303c53f6abade5fd68095641
tetsuji1000 <kontetsu0206@gmail.com>
2021-07-23 20:17:25 +0900

    api/db events

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_032917) do
+ActiveRecord::Schema.define(version: 2021_07_22_111319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2021_06_26_032917) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "eventname", null: false
+    t.datetime "datetime", null: false
+    t.string "cityward", null: false
+    t.string "streetaddress", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +77,5 @@ ActiveRecord::Schema.define(version: 2021_06_26_032917) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "events", "users"
 end
