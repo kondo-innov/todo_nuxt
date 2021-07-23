9890eace588eb407e2758608863b31db7acbfc6c
tetsuji1000 <your.email@example.com>
2021-07-23 20:17:25 +0900

    api/db events

--- api/db/migrate/20210722111319_create_events.rb
+++ api/db/migrate/20210722111319_create_events.rb
@@ -1,6 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.string      :eventname,        null: false
+      t.datetime    :datetime,         null: false
+      t.string      :cityward,         null: false
+      t.string      :streetaddress,    null: false
+      t.text        :description,      null: false
+      t.references  :user,             null: false, foreign_key: true
 
       t.timestamps
     end
