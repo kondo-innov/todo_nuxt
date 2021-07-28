cd4ed21d8bdac6e8b013ff3b01d1e712e80e2247
tetsuji1000 <kontetsu0206@gmail.com>
2021-07-29 06:35:43 +0900

    Not logged in fetchimage　nil eroor Correction

--- api/app/controllers/api/v1/current_users_controller.rb
+++ api/app/controllers/api/v1/current_users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class CurrentUsersController < ApplicationController
       def show
-        if current_user.image.attached?
+        if current_user.present? && current_user.image.attached?
           render json: { url: current_user.image.service_url, messages: '成功' }, stauts: :ok
         else 
           render json: {messages: '失敗' }, stauts: :ok 
