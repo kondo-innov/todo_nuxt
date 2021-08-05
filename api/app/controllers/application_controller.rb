0ad344073736715146cefe0625276f699be6b3ba
tetsuji1000 <kontetsu0206@gmail.com>
2021-08-05 20:04:39 +0900

    api/application.rb edit

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
+  # skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
 end
