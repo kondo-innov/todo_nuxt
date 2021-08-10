9b2637fd505c22c50578a41a7321c2b919cab9fd
tetsuji1000 <your.email@example.com>
2021-08-10 21:33:07 +0900

    api/good edit

--- api/app/controllers/api/v1/likes_controller.rb
+++ api/app/controllers/api/v1/likes_controller.rb
@@ -1,12 +1,17 @@
 module Api
   module V1
     class LikesController < ApplicationController
+      def index
+        likes = current_user.likes
+        render json: likes, status: :ok
+      end
       def create
         @like = Like.new(like_params)
+        @like.user_id = current_user.id
         if @like.save
           render json: @like, status: :ok
         else
-          render json: { status: 400}
+          render json: {message: 'いいねに失敗しました' }, status: 400
         end
       end
 
