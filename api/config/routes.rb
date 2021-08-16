03097776af4bc8bfa9f0dbeb40c548e3b80df949
tetsuji1000 <your.email@example.com>
2021-08-16 20:40:36 +0900

    api/ module endpoint user setting

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :auth do
+        resource :user
+      end
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
           registrations: 'api/v1/auth/registrations',
           sessions: 'api/v1/auth/sessions'
