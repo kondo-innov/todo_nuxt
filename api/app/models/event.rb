0c42772e183be2dfafad2293c089b2491b40aac1
tetsuji1000 <your.email@example.com>
2021-08-19 06:05:43 +0900

    api/event validation　postscript

--- api/app/models/event.rb
+++ api/app/models/event.rb
@@ -5,4 +5,14 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :event_comments, dependent: :destroy
   has_many :joins, dependent: :destroy
+
+  ################################################################################################
+  # バリデーション
+  ################################################################################################
+  validates :eventname, presence: true, length: { maximum: 20 }
+  validates :datetime, presence: true
+  validates :cityward, presence: true
+  validates :streetaddress, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
+
 end
