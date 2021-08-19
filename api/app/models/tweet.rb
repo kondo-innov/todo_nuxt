331a15b7d16c4e6ec0549c75e0cb578cf435e46c
tetsuji1000 <kontetsu0206@gmail.com>
2021-08-19 22:00:05 +0900

    api/model validation front/comment validation

--- api/app/models/tweet.rb
+++ api/app/models/tweet.rb
@@ -1,8 +1,17 @@
 class Tweet < ApplicationRecord
+  ################################################################################################
+  # アソシエーション
+  ################################################################################################
   has_one_attached :picture
 
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :tweet_comments, dependent: :destroy
   has_many :like_lists, dependent: :destroy
+
+  ################################################################################################
+  # バリデーション
+  ################################################################################################
+  validates :description, presence: true, length: { maximum: 100 }
+
 end
