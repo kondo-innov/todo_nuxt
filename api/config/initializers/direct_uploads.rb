f47baa622608ea1cc32d092b366c7897f692c971
tetsuji1000 <kontetsu0206@gmail.com>
2021-08-22 14:36:54 +0900

    api/ remove direct_uploader.rb

--- api/config/initializers/direct_uploads.rb
+++
@@ -1,8 +0,0 @@
-require 'active_storage/direct_uploads_controller'
-
-module ActiveStorage
-  class DirectUploadsController
-    protect_from_forgery with: :null_session
-    skip_before_action :verify_authenticity_token
-  end
-end
